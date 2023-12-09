a76632ac45183527176978d2acf5d49554615881
Ngoding Bang <ngodingbang@gmail.com>
2023-12-09 21:17:16 +0700

    feat: create asyncHandler() function to handle async method of controller on routes

--- app/supports/helpers.js
+++ app/supports/helpers.js
@@ -7,3 +7,15 @@ import { expand } from "dotenv-expand";
 export function loadEnv(options = undefined) {
   expand(config(options));
 }
+
+/**
+ * @param {Function} fn
+ */
+export function asyncHandler(fn) {
+  return function (...args) {
+    /** @type {import("express").NextFunction} */
+    const next = args[args.length - 1];
+
+    return Promise.resolve(fn(...args)).catch(next);
+  };
+}
