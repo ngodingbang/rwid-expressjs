e7b13a5200be40d7b5e8218b687132a9c2efab3d
Ngoding Bang <ngodingbang@gmail.com>
2023-12-21 20:03:30 +0700

    chore: modify error handler response return

--- app/exceptions/handler.js
+++ app/exceptions/handler.js
@@ -20,8 +20,7 @@ export class Handler {
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    res.status(err.status || 500);
-    res.render("error");
+    return res.status(err.status || 500).render("error");
   }
 
   /**
