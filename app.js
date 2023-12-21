b9af007ddf1eb6de472dc8f3510901d8ca62c6aa
Ngoding Bang <ngodingbang@gmail.com>
2023-12-21 20:30:03 +0700

    fix: failed github actions test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { Handler as ErrorHandler } from "./app/exceptions/Handler.js";
 import { Handler as HttpHandler } from "./app/http/Handler.js";
 import { ServiceProvider } from "./app/providers/ServiceProvider.js";
 import { Router } from "./routes/Router.js";
@@ -8,5 +9,6 @@ const app = express();
 new ServiceProvider(app).boot();
 new HttpHandler(app);
 new Router(app);
+new ErrorHandler(app);
 
 export default app;
---
+++ app/exceptions/Handler.js
@@ -0,0 +1,38 @@
+import { NotFoundException } from "./NotFoundException.js";
+
+export class Handler {
+  /**
+   * @param {import("express").Express} app
+   */
+  constructor(app) {
+    app.use(this.notFoundErrorHandler);
+    app.use(this.errorHandler);
+  }
+
+  /**
+   * Catch 404 and forward to error handler.
+   *
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  notFoundErrorHandler(req, res, next) {
+    return next(new NotFoundException());
+  }
+
+  /**
+   * Error handler.
+   *
+   * @param {Error} err
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  // eslint-disable-next-line no-unused-vars
+  errorHandler(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    return res.status(err.status || 500).render("error");
+  }
+}
