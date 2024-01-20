8e8df64a3f0fdb2f77ed3e439dd2d8817c943c49
Fanji Arief <arieffanji@gmail.com>
2024-01-20 08:59:31 +0700

    chore : add router.use(auth)

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,10 +1,12 @@
+import auth from "./auth.js";
 import home from "./home.js";
 import users from "./users.js";
-import express from "express";
+import {Router} from "express";
 
-const router = express.Router();
+const router = Router();
 
 router.use(home);
 router.use(users);
+router.use(auth);
 
 export default router;
