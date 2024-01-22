dfdd03ea55cfbcf770532a6c69751891addcf76a
Fanji Arief <arieffanji@gmail.com>
2024-01-22 14:06:18 +0700

    chore: modify error response using exception on auth.js

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -5,15 +5,11 @@ const router = Router();
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
-  if (username === config.username && password === config.password) {
-    return res.json({
-      message: "succes",
-    });
-  } else {
-    return res.status(401).json({
-      message: "incorrect username or password",
-    });
+  if (!(username === config.username && password === config.password)) {
+    throw new Error("incorrect username or password");
   }
+
+  return res.json({ message: "succes" });
 });
 
 export default router;
